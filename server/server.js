ac5cc156de037181ff001d91841027b16382bf2b
Ezra <ezraakush@gmail.com>
2025-05-03 18:19:12 +0300

    returned to commonjs call method

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-import express from "express";
+const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
