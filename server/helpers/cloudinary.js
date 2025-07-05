9e62f99db235e727047907504de550bdeb2819d0
Ezra <ezraakush@gmail.com>
2025-07-05 17:13:54 +0300

    Update cloudinary.jsremoved last part\

--- server/helpers/cloudinary.js
+++ server/helpers/cloudinary.js
@@ -1,30 +1,27 @@
 const cloudinary = require("cloudinary").v2;
 const multer = require("multer");
 
+// Cloudinary config
 cloudinary.config({
   cloud_name: "dzxrxtphf",
   api_key: "173856312271283",
   api_secret: "w6EJhysPMeU1nWrCVwDODPcvRJ0",
 });
 
+// Multer config
 const storage = new multer.memoryStorage();
+const upload = multer({ storage });
 
+// Async uploader utility
 async function imageUploadUtil(file) {
   const result = await cloudinary.uploader.upload(file, {
     resource_type: "auto",
   });
-
   return result;
 }
 
-const upload = multer({ storage });
-
-module.exports = { upload, imageUploadUtil };
-
-
-
-// write the call code
-
- const result = await cloudinary.uploader.upload(file, {
-   resource_type: "auto",
- });
+// Export
+module.exports = {
+  upload,
+  imageUploadUtil,
+};
